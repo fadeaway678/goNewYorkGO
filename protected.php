ba39878257b1aea23537abc3d16ed0429762f6ad
fadeaway678 <diaryoftano@gmail.com>
2024-06-15 20:01:47 -0400

    Update protected.php

--- protected.php
+++ protected.php
@@ -1,3 +1,8 @@
 <?php
-echo "Welcome to the protected page!";
+  $url = $_GET['https://basketball-stars.io/']; // get the URL from a GET parameter
+  $width = 800;
+  $height = 600;
+  $border = 1;
+
+  echo "<iframe src='$url' width='$width' height='$height' frameborder='$border'></iframe>";
 ?>
